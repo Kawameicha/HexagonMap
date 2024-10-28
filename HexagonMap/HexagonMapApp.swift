fc1bca48025a32da8cb17f9ac1a5fe50a1ac0ac4
Christoph Freier <christoph@freier.fr>
2024-10-28 10:13:39 +0100

    Initial Commit

---
+++ HexagonMap/HexagonMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonMapApp.swift
+//  HexagonMap
+//
+//  Created by Christoph Freier on 28.10.24.
+//
+
+import SwiftUI
+
+@main
+struct HexagonMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
